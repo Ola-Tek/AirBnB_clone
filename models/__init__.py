8baee05b2af105a5c287004909c5fd6ca2d1fc7b
Obisesan Olabode <Obisesanolabode10@gmail.com>
2024-02-13 20:09:41 -0800

    correction on Air_BnB project

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a script showing the magic methods for models directory """
-from models.engine.file_storage import FileStorage
+from engine.file_storage import FileStorage
 
 
 storage = FileStorage()
--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,50 @@
 #!/usr/bin/python3
-"""Shebang script that defines a class model called BaseModel"""
-import models
-from uuid import uuid4()
+"""Defines the BaseModel class."""
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """a general class called the basemodel which contains all attribute and models that needs to be inherited. It defines the base model of the airbnb project"""
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """initializes a new base model
+        """Initialize a new BaseModel.
 
-            Args:
-            *args: they provide space for unused arguments
-            **kwargs: the key values of the dictionary attributes
-            """
-            time_format = Y-%m-%dT%H:%M:%S.%f
-            self.id = str(uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            if len(kwargs) != 0:
-                for a, b in kwargs.items():
-                    if a == "created_at" or a == "updated_at":
-                        self.__dict__[a] = datetime.strptime(b, time_format)
-                    else:
-
-                        self.__dict__[a] = b
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-
-                    models.storage.new(self)
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
-        """update and save updated_at with the current datetime"""
+        """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """returns dictionary containing all keys/values of _dict_of tge instance and also class name of tge obj
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
-        dict_return = self.__dict__.copy()
-        dict_return["created_at"] = self.created_at.isoformat()
-        dict_return["updated_at"] = self.updated_at.isoformat()
-        dict_return["__class__"] = self.__class__.__name__
-            return dict_return
+        ret_dict = self.__dict__.copy()
+        ret_dict["created_at"] = self.created_at.isoformat()
+        ret_dict["updated_at"] = self.updated_at.isoformat()
+        ret_dict["__class__"] = self.__class__.__name__
+        return ret_dict
 
     def __str__(self):
-        """a class that represent the string representation of the class name, id and dict keys and values"""
+        """Return the print/str representation of the BaseModel instance."""
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
